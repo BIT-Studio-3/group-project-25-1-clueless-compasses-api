5cc6a6c1db6f5ed085bf3f9dc363c9c36c18ae71
Joshua Smith <169119042+smitja21@users.noreply.github.com>
2025-05-30 14:41:30 +1200

    hot fix: fixed image upload missing url in staging

--- middleware/uploadMiddleware.js
+++ middleware/uploadMiddleware.js
@@ -1,11 +1,17 @@
 import multer from 'multer';
 import path from 'path';
+import mime from 'mime-types';
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => cb(null, 'uploads/'),
-  filename: (req, file, cb) =>
-    cb(null, Date.now() + path.extname(file.originalname)),
+  filename: (req, file, cb) => {
+    const ext = path.extname(file.originalname);
+    // If original filename has no extension, get extension from mime type
+    const extension = ext || '.' + mime.extension(file.mimetype) || '';
+    cb(null, Date.now() + extension);
+  },
 });
 
 const upload = multer({ storage });
+
 export default upload;
